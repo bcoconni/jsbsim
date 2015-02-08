5bf7d6f858e8e3755ad3cb23a3c2feb2171e404d
bcoconni <bcoconni@users.sourceforge.net>
2015-02-08 15:56:08 +0100

    Broke down most of the test cases in smaller cases that check only one feature at a time. If one test fails it does not prevent the others from running thus getting more information about what is working and what is not.

--- tests/unit_tests/FGColumnVector3Test.h
+++ tests/unit_tests/FGColumnVector3Test.h
@@ -12,7 +12,6 @@ public:
     TS_ASSERT_EQUALS(v0.Entry(3), 0.0);
   }
 
-
   void testConstructorWithLitterals(void) {
     JSBSim::FGColumnVector3 v1(1., 0., -2.);
 
@@ -21,74 +20,155 @@ public:
     TS_ASSERT_EQUALS(v1.Entry(3), -2.0);
   }
 
+  void testOperatorParenthesis(void) {
+    JSBSim::FGColumnVector3 v1(1., 0., -2.);
+
+    TS_ASSERT_EQUALS(v1.Entry(1), v1(1));
+    TS_ASSERT_EQUALS(v1.Entry(2), v1(2));
+    TS_ASSERT_EQUALS(v1.Entry(3), v1(3));
+  }
+
   void testCopyConstructor(void) {
     JSBSim::FGColumnVector3 v1(1., 0., -2.);
     JSBSim::FGColumnVector3 v2(v1);
 
-    // First make sure that v1 and v2 are identical
-    TS_ASSERT_EQUALS(v2.Entry(1), v1.Entry(1));
-    TS_ASSERT_EQUALS(v2.Entry(2), v1.Entry(2));
-    TS_ASSERT_EQUALS(v2.Entry(3), v1.Entry(3));
+    // Check that v1 and v2 are identical
+    TS_ASSERT_EQUALS(v2(1), v1(1));
+    TS_ASSERT_EQUALS(v2(2), v1(2));
+    TS_ASSERT_EQUALS(v2(3), v1(3));
+  }
 
-    // Second, make sure that v1 and v2 are distinct copies
-    // i.e. that v1 and v2 does not point to the same memory location
-    v1.Entry(2) = 5.0;
-    TS_ASSERT_EQUALS(v2.Entry(2), 0.0); // v2[2] must remain unchanged
-    TS_ASSERT_EQUALS(v1.Entry(2), 5.0); // v1[2] must now contain 5.0
+  void testComponentAssignment(void) {
+    JSBSim::FGColumnVector3 v;
+
+    v.Entry(1) = 1.5;
+    TS_ASSERT_EQUALS(v(1), 1.5);
+    TS_ASSERT_EQUALS(v(2), 0.0);
+    TS_ASSERT_EQUALS(v(3), 0.0);
+
+    v.Entry(2) = 2.5;
+    TS_ASSERT_EQUALS(v(1), 1.5);
+    TS_ASSERT_EQUALS(v(2), 2.5);
+    TS_ASSERT_EQUALS(v(3), 0.0);
+
+    v.Entry(3) = -1.5;
+    TS_ASSERT_EQUALS(v(1), 1.5);
+    TS_ASSERT_EQUALS(v(2), 2.5);
+    TS_ASSERT_EQUALS(v(3), -1.5);
+  }
+
+  void testComponentAssignmentOpParent(void) {
+    JSBSim::FGColumnVector3 v;
+
+    v(1) = 1.5;
+    TS_ASSERT_EQUALS(v.Entry(1), 1.5);
+    TS_ASSERT_EQUALS(v.Entry(2), 0.0);
+    TS_ASSERT_EQUALS(v.Entry(3), 0.0);
+
+    v(2) = 2.5;
+    TS_ASSERT_EQUALS(v.Entry(1), 1.5);
+    TS_ASSERT_EQUALS(v.Entry(2), 2.5);
+    TS_ASSERT_EQUALS(v.Entry(3), 0.0);
+
+    v(3) = -1.5;
+    TS_ASSERT_EQUALS(v.Entry(1), 1.5);
+    TS_ASSERT_EQUALS(v.Entry(2), 2.5);
+    TS_ASSERT_EQUALS(v.Entry(3), -1.5);
+  }
+
+  // Check that v1 and v2 are distinct copies i.e. that v1 and v2 does not
+  // point to the same memory location
+  void testCopyConstructorDistinctInstances(void) {
+    JSBSim::FGColumnVector3 v1(1., 0., -2.);
+    JSBSim::FGColumnVector3 v2(v1);
+
+    v1(1) = 5.0;
+    TS_ASSERT_EQUALS(v2(1), 1.0); // v2[1] must remain unchanged
+    v1(2) = 5.0;
+    TS_ASSERT_EQUALS(v2(2), 0.0); // v2[2] must remain unchanged
+    v1(3) = 5.0;
+    TS_ASSERT_EQUALS(v2(3), -2.0); // v2[3] must remain unchanged
   }
 
   void testAssignment(void) {
     JSBSim::FGColumnVector3 v;
-    JSBSim::FGColumnVector3 v1(1., 0., -2.);
-
-    for (unsigned int i=1; i < 4; i++) {
-      TS_ASSERT_EQUALS(v(i), 0.0);
-      TS_ASSERT_EQUALS(v.Entry(i), 0.0);
-    }
+    const JSBSim::FGColumnVector3 v1(1., 0., -2.);
 
     v = v1;
 
-    TS_ASSERT_EQUALS(v1.Entry(1), 1.0);
-    TS_ASSERT_EQUALS(v1.Entry(2), 0.0);
-    TS_ASSERT_EQUALS(v1.Entry(3), -2.0);
+    TS_ASSERT_EQUALS(v1(1), 1.0);
+    TS_ASSERT_EQUALS(v1(2), 0.0);
+    TS_ASSERT_EQUALS(v1(3), -2.0);
+  }
 
-    // Make sure that v and v1 are distinct copies
-    double x = v(2);
-    TS_ASSERT_EQUALS(x, v1.Entry(2));
-    v1.Entry(2) = -8.0;
-    TS_ASSERT_EQUALS(v(2), x);
-    TS_ASSERT_EQUALS(v.Entry(2), x);
+  // Check that the assignment is building distinct copies
+  void testAssignmentDistinctInstances(void) {
+    JSBSim::FGColumnVector3 v;
+    JSBSim::FGColumnVector3 v1(1., 0., -2.);
 
-    for (unsigned int i=1; i < 4; i++) {
+    v = v1;
+
+    v1(1) = -8.0;
+    TS_ASSERT_EQUALS(v(1), 1.0);
+    v1(2) = -8.0;
+    TS_ASSERT_EQUALS(v(2), 0.0);
+    v1(3) = -8.0;
+    TS_ASSERT_EQUALS(v(3), -2.0);
+  }
+
+  // Check that modifying one component has no side effect on the other
+  // components
+  void testAssignmentNoSideEffect(void) {
+    JSBSim::FGColumnVector3 v;
+    const JSBSim::FGColumnVector3 v1(1., 0., -2.);
+
+    for (unsigned int i=1; i <= 3; i++) {
       v = v1;
-      x = v1.Entry(i) + 1.0;
-      v.Entry(i) = x;
-      for (unsigned int j=1; j < 4; j++) {
-        if (i == j) {
-          TS_ASSERT_EQUALS(v(i), x);
-          TS_ASSERT_EQUALS(v.Entry(i), x);
-        }
-        else {
-          TS_ASSERT_EQUALS(v(j), v1.Entry(j));
-          TS_ASSERT_EQUALS(v.Entry(j), v1.Entry(j));
+      double x = v1(i) + 1.0;
+      v(i) = x;
+      for (unsigned int j=1; j <= 3; j++) {
+        if (i != j) {
+          TS_ASSERT_EQUALS(v(j), v1(j));
         }
       }
     }
   }
 
-  void testEquality() {
+  void testEquality(void) {
     JSBSim::FGColumnVector3 v;
-    JSBSim::FGColumnVector3 v1(1., 0., -2.);
+    const JSBSim::FGColumnVector3 v1(1., 0., -2.);
 
     v = v1;
     TS_ASSERT(v == v1);
+  }
 
-    for (unsigned int i=1; i < 4; i++) {
+  // Check that vectors differing by all their components are found inequal
+  void testInequality(void) {
+    const JSBSim::FGColumnVector3 v;
+    const JSBSim::FGColumnVector3 v1(1., 0.5, -2.);
+
+    TS_ASSERT(v != v1);
+  }
+
+  // Check that vectors differing by only one component are found inequal
+  void testInequalityOneComp(void) {
+    JSBSim::FGColumnVector3 v;
+    const JSBSim::FGColumnVector3 v1(1., 0., -2.);
+
+    for (unsigned int i=1; i <= 3; i++) {
       v = v1;
       v(i) = v1.Entry(i) + 1.0;
       TS_ASSERT(v != v1);
+    }
+  }
 
-      for (unsigned int j=1; j < 4; j++) {
+  // Check that vectors differing by two components are found inequal
+  void testInequalityTwoComp(void) {
+    JSBSim::FGColumnVector3 v;
+    const JSBSim::FGColumnVector3 v1(1., 0., -2.);
+
+    for (unsigned int i=1; i <= 3; i++) {
+      for (unsigned int j=1; j <= 3; j++) {
         if (i == j)
           v(i) = v1.Entry(i);
         else
@@ -99,68 +179,133 @@ public:
     }
   }
 
-  void testInitMatrix() {
-    JSBSim::FGColumnVector3 v0;
-    JSBSim::FGColumnVector3 v1(1., 0., -2.);
+  void testInitMatrix(void) {
+    const JSBSim::FGColumnVector3 v0;
+    JSBSim::FGColumnVector3 v1(1., 0.5, -2.);
 
     v1.InitMatrix();
     TS_ASSERT(v1 == v0);
-
-    v1.InitMatrix(1.0);
-    TS_ASSERT_EQUALS(v1(1), 1.0);
-    TS_ASSERT_EQUALS(v1(2), 1.0);
-    TS_ASSERT_EQUALS(v1(3), 1.0);
-
-    v1.InitMatrix(-1.0, 2.0, 0.0);
-    TS_ASSERT_EQUALS(v1(1), -1.0);
-    TS_ASSERT_EQUALS(v1(2), 2.0);
-    TS_ASSERT_EQUALS(v1(3), 0.0);
   }
 
-  void testOperations() {
-    JSBSim::FGColumnVector3 v0;
-    JSBSim::FGColumnVector3 v1(1., 0., -2.);
-    JSBSim::FGColumnVector3 v_twice = 2.0 * v1;
+  void testInitMatrixOneParam(void) {
+    JSBSim::FGColumnVector3 v;
 
-    TS_ASSERT_EQUALS(v_twice(1), 2.0 * v1(1));
-    TS_ASSERT_EQUALS(v_twice(2), 2.0 * v1(2));
-    TS_ASSERT_EQUALS(v_twice(3), 2.0 * v1(3));
-    TS_ASSERT(v_twice == v1 * 2.0);
-    TS_ASSERT(v_twice / 2.0 == v1);
-    TS_ASSERT(v_twice / 0.0 == v0);
-    TS_ASSERT(v_twice + v1 == 3.0 * v1);
-    TS_ASSERT(v_twice - v1 == v1);
-    v_twice /= 2.0;
-    TS_ASSERT(v_twice == v1);
-    v_twice.InitMatrix(1.0);
-    v_twice *= -1.0;
-    TS_ASSERT_EQUALS(v_twice(1), -1.0);
-    TS_ASSERT_EQUALS(v_twice(2), -1.0);
-    TS_ASSERT_EQUALS(v_twice(3), -1.0);
-
-    JSBSim::FGColumnVector3 v(1.0, -2.0, 0.0);
-    v += v_twice;
-    TS_ASSERT_EQUALS(v(1), 0.0);
-    TS_ASSERT_EQUALS(v(2), -3.0);
-    TS_ASSERT_EQUALS(v(3), -1.0);
-
-    v -= v_twice;
+    v.InitMatrix(1.0);
     TS_ASSERT_EQUALS(v(1), 1.0);
-    TS_ASSERT_EQUALS(v(2), -2.0);
-    TS_ASSERT_EQUALS(v(3), 0.0);
+    TS_ASSERT_EQUALS(v(2), 1.0);
+    TS_ASSERT_EQUALS(v(3), 1.0);
+  }
 
-    v.InitMatrix(1.0, -2.0, 0.5);
-    TS_ASSERT_EQUALS(DotProduct(v,v), 5.25);
-    v /= 0.0;
-    TS_ASSERT_EQUALS(v(1), 1.0);
-    TS_ASSERT_EQUALS(v(2), -2.0);
+  void testInitMatrixTwoParam(void) {
+    JSBSim::FGColumnVector3 v;
+
+    v.InitMatrix(-1.0, 2.0, 0.5);
+    TS_ASSERT_EQUALS(v(1), -1.0);
+    TS_ASSERT_EQUALS(v(2), 2.0);
     TS_ASSERT_EQUALS(v(3), 0.5);
   }
 
-  void testCrossProduct() {
-    JSBSim::FGColumnVector3 X(1.0,0.0,0.0);
-    JSBSim::FGColumnVector3 Y(0.0,1.0,0.0);
-    JSBSim::FGColumnVector3 Z(0.0,0.0,1.0);
+  void testLhsScalarMultiplication(void) {
+    const JSBSim::FGColumnVector3 v(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v_twice = 2.0 * v;
+
+    TS_ASSERT_EQUALS(v_twice(1), 2.0 * v(1));
+    TS_ASSERT_EQUALS(v_twice(2), 2.0 * v(2));
+    TS_ASSERT_EQUALS(v_twice(3), 2.0 * v(3));
+  }
+
+  void testRhsScalarMultiplication(void) {
+    const JSBSim::FGColumnVector3 v(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v_twice = v * 2.0;
+
+    TS_ASSERT_EQUALS(v_twice, 2.0 * v);
+  }
+
+  void testScalarDivision(void) {
+    const JSBSim::FGColumnVector3 v(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v_half = v / 2.0;
+
+    TS_ASSERT_EQUALS(v_half, 0.5 * v);
+  }
+
+  void testDivisionByZero(void) {
+    const JSBSim::FGColumnVector3 v0;
+    const JSBSim::FGColumnVector3 v(1., 0., -2.);
+
+    TS_ASSERT_EQUALS(v / 0.0, v0);
+  }
+
+  void testAddition(void) {
+    const JSBSim::FGColumnVector3 v(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v_twice = v * 2.0;
+
+    TS_ASSERT_EQUALS(v_twice + v, 3.0 * v);
+  }
+
+  void testSubtraction(void) {
+    const JSBSim::FGColumnVector3 v(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v_twice = v * 2.0;
+
+    TS_ASSERT_EQUALS(v_twice - v, v);
+  }
+
+  void testAdditionAssignment(void) {
+    const JSBSim::FGColumnVector3 v(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v_twice(v);
+
+    v_twice += v;
+    TS_ASSERT_EQUALS(v_twice, 2.0 * v);
+  }
+
+  void testSubtractionAssignment(void) {
+    const JSBSim::FGColumnVector3 v(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v_twice = 2.0 * v;
+
+    v_twice -= v;
+    TS_ASSERT_EQUALS(v_twice, v);
+  }
+
+  void testScalarMultiplicationAssignment(void) {
+    const JSBSim::FGColumnVector3 v(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v_twice(v);
+
+    v_twice *= 2.0;
+    TS_ASSERT_EQUALS(v_twice, 2.0 * v);
+  }
+
+  void testScalarDivisionAssignment(void) {
+    const JSBSim::FGColumnVector3 v(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v_half(v);
+
+    v_half /= 2.0;
+    TS_ASSERT_EQUALS(v_half, 0.5 * v);
+  }
+
+  void testDivisionByZeroAssignment(void) {
+    const JSBSim::FGColumnVector3 v(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v1(v);
+
+    v1 /= 0.0;
+    TS_ASSERT_EQUALS(v1, v);
+  }
+
+  void testDotProduct(void) {
+    const JSBSim::FGColumnVector3 v(1.0, -2.0, 0.5);
+    const JSBSim::FGColumnVector3 X(1.0,0.0,0.0);
+    const JSBSim::FGColumnVector3 Y(0.0,1.0,0.0);
+    const JSBSim::FGColumnVector3 Z(0.0,0.0,1.0);
+
+    TS_ASSERT_EQUALS(DotProduct(v,v), 5.25);
+    TS_ASSERT_EQUALS(DotProduct(X, Y), 0.0);
+    TS_ASSERT_EQUALS(DotProduct(Y, Z), 0.0);
+    TS_ASSERT_EQUALS(DotProduct(X, Z), 0.0);
+  }
+
+  void testCrossProduct(void) {
+    const JSBSim::FGColumnVector3 v0;
+    const JSBSim::FGColumnVector3 X(1.0,0.0,0.0);
+    const JSBSim::FGColumnVector3 Y(0.0,1.0,0.0);
+    const JSBSim::FGColumnVector3 Z(0.0,0.0,1.0);
 
     TS_ASSERT_EQUALS(X * Y, Z);
     TS_ASSERT_EQUALS(Y * X, -1.0 * Z);
@@ -168,22 +313,28 @@ public:
     TS_ASSERT_EQUALS(Z * Y, -1.0 * X);
     TS_ASSERT_EQUALS(Z * X, Y);
     TS_ASSERT_EQUALS(X * Z, -1.0 * Y);
-    TS_ASSERT_EQUALS(DotProduct(X, Y), 0.0);
-    TS_ASSERT_EQUALS(DotProduct(Y, Z), 0.0);
-    TS_ASSERT_EQUALS(DotProduct(X, Z), 0.0);
-}
+    TS_ASSERT_EQUALS(X * X, v0);
+    TS_ASSERT_EQUALS(Y * Y, v0);
+    TS_ASSERT_EQUALS(Z * Z, v0);
+  }
 
-  void testNorm() {
-    JSBSim::FGColumnVector3 v0;
-    JSBSim::FGColumnVector3 v(3.0, 4.0, 0.0);
+  void testMagnitude(void) {
+    const JSBSim::FGColumnVector3 v0;
+    const JSBSim::FGColumnVector3 v(3.0, 4.0, 0.0);
 
     TS_ASSERT_EQUALS(v0.Magnitude(), 0.0);
-    TS_ASSERT_EQUALS(v0.Normalize(), v0);
     TS_ASSERT_EQUALS(v.Magnitude(1,3), 3.0);
     TS_ASSERT_EQUALS(v.Magnitude(2,3), 4.0);
     TS_ASSERT_EQUALS(v.Magnitude(1,2), 5.0);
     TS_ASSERT_EQUALS(v.Magnitude(), 5.0);
     TS_ASSERT_EQUALS(DotProduct(v,v), v.Magnitude() * v.Magnitude());
+  }
+
+  void testNormalize(void) {
+    JSBSim::FGColumnVector3 v0;
+    JSBSim::FGColumnVector3 v(3.0, 4.0, 0.0);
+
+    TS_ASSERT_EQUALS(v0.Normalize(), v0);
 
     v.Normalize();
     TS_ASSERT_DELTA(v(1), 0.6, 1E-9);
@@ -191,8 +342,8 @@ public:
     TS_ASSERT_EQUALS(v(3), 0.0);
   }
 
-  void testOutput() {
-    JSBSim::FGColumnVector3 v1(1., 0., -2.);
+  void testOutput(void) {
+    const JSBSim::FGColumnVector3 v1(1., 0., -2.);
     std::string s = v1.Dump(" , ");
 
     TS_ASSERT_EQUALS(s, std::string("1 , 0 , -2"));
