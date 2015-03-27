23e6474aa598e89a9940cbd28fffe99ac9cbca44
bcoconni <bcoconni>
2015-03-28 00:18:55 +0100

    Reinforced the tests for FGColumnVector3

--- tests/unit_tests/FGColumnVector3Test.h
+++ tests/unit_tests/FGColumnVector3Test.h
@@ -4,8 +4,8 @@
 class FGColumnVector3Test : public CxxTest::TestSuite
 {
 public:
-  void testNullity(void) {
-    const JSBSim::FGColumnVector3 v0;
+  void testDefaultConstructor(void) {
+    JSBSim::FGColumnVector3 v0;
 
     TS_ASSERT_EQUALS(v0.Entry(1), 0.0);
     TS_ASSERT_EQUALS(v0.Entry(2), 0.0);
@@ -20,12 +20,12 @@ public:
     TS_ASSERT_EQUALS(v1.Entry(3), -2.0);
   }
 
-  void testOperatorParenthesis(void) {
+  void testParenthesisOperator(void) {
     JSBSim::FGColumnVector3 v1(1., 0., -2.);
 
-    TS_ASSERT_EQUALS(v1.Entry(1), v1(1));
-    TS_ASSERT_EQUALS(v1.Entry(2), v1(2));
-    TS_ASSERT_EQUALS(v1.Entry(3), v1(3));
+    TS_ASSERT_EQUALS(v1(1), 1.0);
+    TS_ASSERT_EQUALS(v1(2), 0.0);
+    TS_ASSERT_EQUALS(v1(3), -2.0);
   }
 
   void testCopyConstructor(void) {
@@ -41,17 +41,17 @@ public:
   void testComponentAssignment(void) {
     JSBSim::FGColumnVector3 v;
 
-    v.Entry(1) = 1.5;
+    v(1) = 1.5;
     TS_ASSERT_EQUALS(v(1), 1.5);
     TS_ASSERT_EQUALS(v(2), 0.0);
     TS_ASSERT_EQUALS(v(3), 0.0);
 
-    v.Entry(2) = 2.5;
+    v(2) = 2.5;
     TS_ASSERT_EQUALS(v(1), 1.5);
     TS_ASSERT_EQUALS(v(2), 2.5);
     TS_ASSERT_EQUALS(v(3), 0.0);
 
-    v.Entry(3) = -1.5;
+    v(3) = -1.5;
     TS_ASSERT_EQUALS(v(1), 1.5);
     TS_ASSERT_EQUALS(v(2), 2.5);
     TS_ASSERT_EQUALS(v(3), -1.5);
@@ -76,6 +76,27 @@ public:
     TS_ASSERT_EQUALS(v.Entry(3), -1.5);
   }
 
+  // Check that modifying one component has no side effect on the other
+  // components
+  void testComponentAssignmentNoSideEffect(void) {
+    JSBSim::FGColumnVector3 v;
+    JSBSim::FGColumnVector3 v1(1., 0., -2.);
+
+    for (unsigned int i=1; i <= 3; i++) {
+      v = v1;
+      double x = v1(i) + 1.0;
+      v(i) = x;
+      for (unsigned int j=1; j <= 3; j++) {
+        if (i != j) {
+          TS_ASSERT_EQUALS(v(j), v1(j));
+        }
+        else {
+          TS_ASSERT_EQUALS(v(j), x);
+        }
+      }
+    }
+  }
+
   // Check that v1 and v2 are distinct copies i.e. that v1 and v2 does not
   // point to the same memory location
   void testCopyConstructorDistinctInstances(void) {
@@ -92,10 +113,15 @@ public:
 
   void testAssignment(void) {
     JSBSim::FGColumnVector3 v;
-    const JSBSim::FGColumnVector3 v1(1., 0., -2.);
+    JSBSim::FGColumnVector3 v1(1., 0., -2.);
 
     v = v1;
 
+    TS_ASSERT_EQUALS(v(1), 1.0);
+    TS_ASSERT_EQUALS(v(2), 0.0);
+    TS_ASSERT_EQUALS(v(3), -2.0);
+
+    // Verify that the operand is not modified
     TS_ASSERT_EQUALS(v1(1), 1.0);
     TS_ASSERT_EQUALS(v1(2), 0.0);
     TS_ASSERT_EQUALS(v1(3), -2.0);
@@ -116,29 +142,10 @@ public:
     TS_ASSERT_EQUALS(v(3), -2.0);
   }
 
-  // Check that modifying one component has no side effect on the other
-  // components
-  void testAssignmentNoSideEffect(void) {
-    JSBSim::FGColumnVector3 v;
-    const JSBSim::FGColumnVector3 v1(1., 0., -2.);
-
-    for (unsigned int i=1; i <= 3; i++) {
-      v = v1;
-      double x = v1(i) + 1.0;
-      v(i) = x;
-      for (unsigned int j=1; j <= 3; j++) {
-        if (i != j) {
-          TS_ASSERT_EQUALS(v(j), v1(j));
-        }
-      }
-    }
-  }
-
   void testEquality(void) {
-    JSBSim::FGColumnVector3 v;
-    const JSBSim::FGColumnVector3 v1(1., 0., -2.);
+    JSBSim::FGColumnVector3 v(1., 0., -2.);
+    JSBSim::FGColumnVector3 v1(v);
 
-    v = v1;
     TS_ASSERT(v == v1);
   }
 
@@ -184,7 +191,7 @@ public:
     JSBSim::FGColumnVector3 v1(1., 0.5, -2.);
 
     v1.InitMatrix();
-    TS_ASSERT(v1 == v0);
+    TS_ASSERT_EQUALS(v1, v0);
   }
 
   void testInitMatrixOneParam(void) {
@@ -196,7 +203,7 @@ public:
     TS_ASSERT_EQUALS(v(3), 1.0);
   }
 
-  void testInitMatrixTwoParam(void) {
+  void testInitMatrixThreeParams(void) {
     JSBSim::FGColumnVector3 v;
 
     v.InitMatrix(-1.0, 2.0, 0.5);
@@ -206,26 +213,77 @@ public:
   }
 
   void testLhsScalarMultiplication(void) {
-    const JSBSim::FGColumnVector3 v(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v(1., 0.5, -2.);
     JSBSim::FGColumnVector3 v_twice = 2.0 * v;
 
-    TS_ASSERT_EQUALS(v_twice(1), 2.0 * v(1));
-    TS_ASSERT_EQUALS(v_twice(2), 2.0 * v(2));
-    TS_ASSERT_EQUALS(v_twice(3), 2.0 * v(3));
+    TS_ASSERT_EQUALS(v_twice(1), 2.0);
+    TS_ASSERT_EQUALS(v_twice(2), 1.0);
+    TS_ASSERT_EQUALS(v_twice(3), -4.0);
+
+    // Verify that the operand is not modified
+    TS_ASSERT_EQUALS(v(1), 1.0);
+    TS_ASSERT_EQUALS(v(2), 0.5);
+    TS_ASSERT_EQUALS(v(3), -2.0);
+  }
+
+  // Check the scalar multiplication when the operand is on both sides of the
+  // assignment
+  void testLhsScalarMultiplicationSelfRef(void) {
+    JSBSim::FGColumnVector3 v(1., 0.5, -2.);
+    v = 2.0 * v;
+
+    TS_ASSERT_EQUALS(v(1), 2.0);
+    TS_ASSERT_EQUALS(v(2), 1.0);
+    TS_ASSERT_EQUALS(v(3), -4.0);
   }
 
   void testRhsScalarMultiplication(void) {
-    const JSBSim::FGColumnVector3 v(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v(1., 0.5, -2.);
     JSBSim::FGColumnVector3 v_twice = v * 2.0;
 
-    TS_ASSERT_EQUALS(v_twice, 2.0 * v);
+    TS_ASSERT_EQUALS(v_twice(1), 2.0);
+    TS_ASSERT_EQUALS(v_twice(2), 1.0);
+    TS_ASSERT_EQUALS(v_twice(3), -4.0);
+
+    // Verify that the operand is not modified
+    TS_ASSERT_EQUALS(v(1), 1.0);
+    TS_ASSERT_EQUALS(v(2), 0.5);
+    TS_ASSERT_EQUALS(v(3), -2.0);
+  }
+
+  // Check the scalar multiplication when the operand is on both sides of the
+  // assignment
+  void testRhsScalarMultiplicationSelfRef(void) {
+    JSBSim::FGColumnVector3 v(1., 0.5, -2.);
+    v = v * 2.0;
+
+    TS_ASSERT_EQUALS(v(1), 2.0);
+    TS_ASSERT_EQUALS(v(2), 1.0);
+    TS_ASSERT_EQUALS(v(3), -4.0);
   }
 
   void testScalarDivision(void) {
-    const JSBSim::FGColumnVector3 v(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v(1., 0.5, -2.);
     JSBSim::FGColumnVector3 v_half = v / 2.0;
 
     TS_ASSERT_EQUALS(v_half, 0.5 * v);
+
+    // Verify that the operand is not modified
+    TS_ASSERT_EQUALS(v(1), 1.0);
+    TS_ASSERT_EQUALS(v(2), 0.5);
+    TS_ASSERT_EQUALS(v(3), -2.0);
+  }
+
+  // Check the scalar division when the operand is on both sides of the
+  // assignment
+  void testScalarDivisionSelfRef(void) {
+    JSBSim::FGColumnVector3 v(1., 0.5, -2.);
+
+    v = v / 2.0;
+
+    TS_ASSERT_EQUALS(v(1), 0.5);
+    TS_ASSERT_EQUALS(v(2), 0.25);
+    TS_ASSERT_EQUALS(v(3), -1.0);
   }
 
   void testDivisionByZero(void) {
@@ -236,76 +294,245 @@ public:
   }
 
   void testAddition(void) {
-    const JSBSim::FGColumnVector3 v(1., 0.5, -2.);
-    JSBSim::FGColumnVector3 v_twice = v * 2.0;
+    JSBSim::FGColumnVector3 v1(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v2(0., 1., 2.);
+    JSBSim::FGColumnVector3 v = v1 + v2;
 
-    TS_ASSERT_EQUALS(v_twice + v, 3.0 * v);
+    TS_ASSERT_EQUALS(v(1), 1.0);
+    TS_ASSERT_EQUALS(v(2), 1.5);
+    TS_ASSERT_EQUALS(v(3), 0.0);
+
+    // Verify that the operands are not modified
+    TS_ASSERT_EQUALS(v1(1), 1.0);
+    TS_ASSERT_EQUALS(v1(2), 0.5);
+    TS_ASSERT_EQUALS(v1(3), -2.0);
+
+    TS_ASSERT_EQUALS(v2(1), 0.0);
+    TS_ASSERT_EQUALS(v2(2), 1.0);
+    TS_ASSERT_EQUALS(v2(3), 2.0);
+  }
+
+  // Check the addition when the operand is on both sides of the assignment
+  void testAdditionSelfRefLeft(void) {
+    JSBSim::FGColumnVector3 v1(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v2(0., 1., 2.);
+    v1 = v1 + v2;
+
+    TS_ASSERT_EQUALS(v1(1), 1.0);
+    TS_ASSERT_EQUALS(v1(2), 1.5);
+    TS_ASSERT_EQUALS(v1(3), 0.0);
+
+    // Verify that the other operand is not modified
+    TS_ASSERT_EQUALS(v2(1), 0.0);
+    TS_ASSERT_EQUALS(v2(2), 1.0);
+    TS_ASSERT_EQUALS(v2(3), 2.0);
+  }
+
+  // Check the addition when the operand is on both sides of the assignment
+  void testAdditionSelfRefRight(void) {
+    JSBSim::FGColumnVector3 v1(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v2(0., 1., 2.);
+    v1 = v2 + v1;
+
+    TS_ASSERT_EQUALS(v1(1), 1.0);
+    TS_ASSERT_EQUALS(v1(2), 1.5);
+    TS_ASSERT_EQUALS(v1(3), 0.0);
+
+    // Verify that the other operand is not modified
+    TS_ASSERT_EQUALS(v2(1), 0.0);
+    TS_ASSERT_EQUALS(v2(2), 1.0);
+    TS_ASSERT_EQUALS(v2(3), 2.0);
+  }
+
+  // Check the addition when the operand is on both sides of the assignment
+  void testAdditionSelfSelf(void) {
+    JSBSim::FGColumnVector3 v1(1., 0.5, -2.);
+    v1 = v1 + v1;
+
+    TS_ASSERT_EQUALS(v1(1), 2.0);
+    TS_ASSERT_EQUALS(v1(2), 1.0);
+    TS_ASSERT_EQUALS(v1(3), -4.0);
   }
 
   void testSubtraction(void) {
-    const JSBSim::FGColumnVector3 v(1., 0.5, -2.);
-    JSBSim::FGColumnVector3 v_twice = v * 2.0;
+    JSBSim::FGColumnVector3 v1(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v2(0., 1., 2.);
+    JSBSim::FGColumnVector3 v = v1 - v2;
 
-    TS_ASSERT_EQUALS(v_twice - v, v);
+    TS_ASSERT_EQUALS(v(1), 1.0);
+    TS_ASSERT_EQUALS(v(2), -0.5);
+    TS_ASSERT_EQUALS(v(3), -4.0);
+
+    // Verify that the operands are not modified
+    TS_ASSERT_EQUALS(v1(1), 1.0);
+    TS_ASSERT_EQUALS(v1(2), 0.5);
+    TS_ASSERT_EQUALS(v1(3), -2.0);
+
+    TS_ASSERT_EQUALS(v2(1), 0.0);
+    TS_ASSERT_EQUALS(v2(2), 1.0);
+    TS_ASSERT_EQUALS(v2(3), 2.0);
+  }
+
+  // Check the subtraction when the operand is on both sides of the assignment
+  void testSubtractionSelfLeft(void) {
+    JSBSim::FGColumnVector3 v1(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v2(0., 1., 2.);
+    v1 = v1 - v2;
+
+    TS_ASSERT_EQUALS(v1(1), 1.0);
+    TS_ASSERT_EQUALS(v1(2), -0.5);
+    TS_ASSERT_EQUALS(v1(3), -4.0);
+
+    // Verify that the operands are not modified
+    TS_ASSERT_EQUALS(v2(1), 0.0);
+    TS_ASSERT_EQUALS(v2(2), 1.0);
+    TS_ASSERT_EQUALS(v2(3), 2.0);
+  }
+
+  // Check the subtraction when the operand is on both sides of the assignment
+  void testSubtractionSelfRight(void) {
+    JSBSim::FGColumnVector3 v1(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v2(0., 1., 2.);
+    v1 = v2 - v1;
+
+    TS_ASSERT_EQUALS(v1(1), -1.0);
+    TS_ASSERT_EQUALS(v1(2), 0.5);
+    TS_ASSERT_EQUALS(v1(3), 4.0);
+
+    // Verify that the operands are not modified
+    TS_ASSERT_EQUALS(v2(1), 0.0);
+    TS_ASSERT_EQUALS(v2(2), 1.0);
+    TS_ASSERT_EQUALS(v2(3), 2.0);
+  }
+
+  // Check the subtraction when the operand is on both sides of the assignment
+  void testSubtractionSelfSelf(void) {
+    JSBSim::FGColumnVector3 v1(1., 0.5, -2.);
+    v1 = v1 - v1;
+
+    TS_ASSERT_EQUALS(v1(1), 0.0);
+    TS_ASSERT_EQUALS(v1(2), 0.0);
+    TS_ASSERT_EQUALS(v1(3), 0.0);
   }
 
   void testAdditionAssignment(void) {
-    const JSBSim::FGColumnVector3 v(1., 0.5, -2.);
-    JSBSim::FGColumnVector3 v_twice(v);
+    JSBSim::FGColumnVector3 v(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v2(0., 1., 2.);
 
-    v_twice += v;
-    TS_ASSERT_EQUALS(v_twice, 2.0 * v);
+    v += v2;
+
+    TS_ASSERT_EQUALS(v(1), 1.0);
+    TS_ASSERT_EQUALS(v(2), 1.5);
+    TS_ASSERT_EQUALS(v(3), 0.0);
+
+    // Verify that the operand is not modified
+    TS_ASSERT_EQUALS(v2(1), 0.0);
+    TS_ASSERT_EQUALS(v2(2), 1.0);
+    TS_ASSERT_EQUALS(v2(3), 2.0);
+  }
+
+  // Check the operation when the operand is on both sides of the assignment
+  void testAdditionAssignmentSelf(void) {
+    JSBSim::FGColumnVector3 v(1., 0.5, -2.);
+
+    v += v;
+
+    TS_ASSERT_EQUALS(v(1), 2.0);
+    TS_ASSERT_EQUALS(v(2), 1.0);
+    TS_ASSERT_EQUALS(v(3), -4.0);
   }
 
   void testSubtractionAssignment(void) {
-    const JSBSim::FGColumnVector3 v(1., 0.5, -2.);
-    JSBSim::FGColumnVector3 v_twice = 2.0 * v;
+    JSBSim::FGColumnVector3 v(1., 0.5, -2.);
+    JSBSim::FGColumnVector3 v2(0., 1., 2.);
 
-    v_twice -= v;
-    TS_ASSERT_EQUALS(v_twice, v);
+    v -= v2;
+
+    TS_ASSERT_EQUALS(v(1), 1.0);
+    TS_ASSERT_EQUALS(v(2), -0.5);
+    TS_ASSERT_EQUALS(v(3), -4.0);
+
+    // Verify that the operand is not modified
+    TS_ASSERT_EQUALS(v2(1), 0.0);
+    TS_ASSERT_EQUALS(v2(2), 1.0);
+    TS_ASSERT_EQUALS(v2(3), 2.0);
+  }
+
+  // Check the operation when the operand is on both sides of the assignment
+  void testSubtractionAssignmentSelf(void) {
+    JSBSim::FGColumnVector3 v(1., 0.5, -2.);
+
+    v -= v;
+
+    TS_ASSERT_EQUALS(v(1), 0.0);
+    TS_ASSERT_EQUALS(v(2), 0.0);
+    TS_ASSERT_EQUALS(v(3), 0.0);
   }
 
   void testScalarMultiplicationAssignment(void) {
-    const JSBSim::FGColumnVector3 v(1., 0.5, -2.);
-    JSBSim::FGColumnVector3 v_twice(v);
+    JSBSim::FGColumnVector3 v(1., 0.5, -2.);
 
-    v_twice *= 2.0;
-    TS_ASSERT_EQUALS(v_twice, 2.0 * v);
+    v *= 2.0;
+
+    TS_ASSERT_EQUALS(v(1), 2.0);
+    TS_ASSERT_EQUALS(v(2), 1.0);
+    TS_ASSERT_EQUALS(v(3), -4.0);
   }
 
   void testScalarDivisionAssignment(void) {
-    const JSBSim::FGColumnVector3 v(1., 0.5, -2.);
-    JSBSim::FGColumnVector3 v_half(v);
+    JSBSim::FGColumnVector3 v(1., 0.5, -2.);
 
-    v_half /= 2.0;
-    TS_ASSERT_EQUALS(v_half, 0.5 * v);
+    v /= 2.0;
+
+    TS_ASSERT_EQUALS(v(1), 0.5);
+    TS_ASSERT_EQUALS(v(2), 0.25);
+    TS_ASSERT_EQUALS(v(3), -1.0);
   }
 
   void testDivisionByZeroAssignment(void) {
-    const JSBSim::FGColumnVector3 v(1., 0.5, -2.);
-    JSBSim::FGColumnVector3 v1(v);
+    JSBSim::FGColumnVector3 v(1., 0.5, -2.);
 
-    v1 /= 0.0;
-    TS_ASSERT_EQUALS(v1, v);
+    v /= 0.0;
+
+    TS_ASSERT_EQUALS(v(1), 1.0);
+    TS_ASSERT_EQUALS(v(2), 0.5);
+    TS_ASSERT_EQUALS(v(3), -2.0);
   }
 
   void testDotProduct(void) {
-    const JSBSim::FGColumnVector3 v(1.0, -2.0, 0.5);
-    const JSBSim::FGColumnVector3 X(1.0,0.0,0.0);
-    const JSBSim::FGColumnVector3 Y(0.0,1.0,0.0);
-    const JSBSim::FGColumnVector3 Z(0.0,0.0,1.0);
+    JSBSim::FGColumnVector3 v(1.0, -2.0, 0.5);
+    JSBSim::FGColumnVector3 X(1.0,0.0,0.0);
+    JSBSim::FGColumnVector3 Y(0.0,1.0,0.0);
+    JSBSim::FGColumnVector3 Z(0.0,0.0,1.0);
 
     TS_ASSERT_EQUALS(DotProduct(v,v), 5.25);
     TS_ASSERT_EQUALS(DotProduct(X, Y), 0.0);
     TS_ASSERT_EQUALS(DotProduct(Y, Z), 0.0);
     TS_ASSERT_EQUALS(DotProduct(X, Z), 0.0);
+
+    // Verify that the operands are not modified
+    TS_ASSERT_EQUALS(v(1), 1.0);
+    TS_ASSERT_EQUALS(v(2), -2.0);
+    TS_ASSERT_EQUALS(v(3), 0.5);
+
+    TS_ASSERT_EQUALS(X(1), 1.0);
+    TS_ASSERT_EQUALS(X(2), 0.0);
+    TS_ASSERT_EQUALS(X(3), 0.0);
+
+    TS_ASSERT_EQUALS(Y(1), 0.0);
+    TS_ASSERT_EQUALS(Y(2), 1.0);
+    TS_ASSERT_EQUALS(Y(3), 0.0);
+
+    TS_ASSERT_EQUALS(Z(1), 0.0);
+    TS_ASSERT_EQUALS(Z(2), 0.0);
+    TS_ASSERT_EQUALS(Z(3), 1.0);
   }
 
   void testCrossProduct(void) {
-    const JSBSim::FGColumnVector3 v0;
-    const JSBSim::FGColumnVector3 X(1.0,0.0,0.0);
-    const JSBSim::FGColumnVector3 Y(0.0,1.0,0.0);
-    const JSBSim::FGColumnVector3 Z(0.0,0.0,1.0);
+    JSBSim::FGColumnVector3 v0;
+    JSBSim::FGColumnVector3 X(1.0,0.0,0.0);
+    JSBSim::FGColumnVector3 Y(0.0,1.0,0.0);
+    JSBSim::FGColumnVector3 Z(0.0,0.0,1.0);
 
     TS_ASSERT_EQUALS(X * Y, Z);
     TS_ASSERT_EQUALS(Y * X, -1.0 * Z);
@@ -316,11 +543,28 @@ public:
     TS_ASSERT_EQUALS(X * X, v0);
     TS_ASSERT_EQUALS(Y * Y, v0);
     TS_ASSERT_EQUALS(Z * Z, v0);
+
+    // Verify that the operands are not modified
+    TS_ASSERT_EQUALS(v0(1), 0.0);
+    TS_ASSERT_EQUALS(v0(2), 0.0);
+    TS_ASSERT_EQUALS(v0(3), 0.0);
+
+    TS_ASSERT_EQUALS(X(1), 1.0);
+    TS_ASSERT_EQUALS(X(2), 0.0);
+    TS_ASSERT_EQUALS(X(3), 0.0);
+
+    TS_ASSERT_EQUALS(Y(1), 0.0);
+    TS_ASSERT_EQUALS(Y(2), 1.0);
+    TS_ASSERT_EQUALS(Y(3), 0.0);
+
+    TS_ASSERT_EQUALS(Z(1), 0.0);
+    TS_ASSERT_EQUALS(Z(2), 0.0);
+    TS_ASSERT_EQUALS(Z(3), 1.0);
   }
 
   void testMagnitude(void) {
-    const JSBSim::FGColumnVector3 v0;
-    const JSBSim::FGColumnVector3 v(3.0, 4.0, 0.0);
+    JSBSim::FGColumnVector3 v0;
+    JSBSim::FGColumnVector3 v(3.0, 4.0, 0.0);
 
     TS_ASSERT_EQUALS(v0.Magnitude(), 0.0);
     TS_ASSERT_EQUALS(v.Magnitude(1,3), 3.0);
@@ -328,13 +572,25 @@ public:
     TS_ASSERT_EQUALS(v.Magnitude(1,2), 5.0);
     TS_ASSERT_EQUALS(v.Magnitude(), 5.0);
     TS_ASSERT_EQUALS(DotProduct(v,v), v.Magnitude() * v.Magnitude());
+
+    // Verify that the operands are not modified
+    TS_ASSERT_EQUALS(v0(1), 0.0);
+    TS_ASSERT_EQUALS(v0(2), 0.0);
+    TS_ASSERT_EQUALS(v0(3), 0.0);
+
+    TS_ASSERT_EQUALS(v(1), 3.0);
+    TS_ASSERT_EQUALS(v(2), 4.0);
+    TS_ASSERT_EQUALS(v(3), 0.0);
   }
 
   void testNormalize(void) {
     JSBSim::FGColumnVector3 v0;
     JSBSim::FGColumnVector3 v(3.0, 4.0, 0.0);
 
-    TS_ASSERT_EQUALS(v0.Normalize(), v0);
+    v0.Normalize();
+    TS_ASSERT_EQUALS(v0(1), 0.0);
+    TS_ASSERT_EQUALS(v0(2), 0.0);
+    TS_ASSERT_EQUALS(v0(3), 0.0);
 
     v.Normalize();
     TS_ASSERT_DELTA(v(1), 0.6, 1E-9);
@@ -343,12 +599,17 @@ public:
   }
 
   void testOutput(void) {
-    const JSBSim::FGColumnVector3 v1(1., 0., -2.);
+    JSBSim::FGColumnVector3 v1(1., 0., -2.);
     std::string s = v1.Dump(" , ");
 
     TS_ASSERT_EQUALS(s, std::string("1 , 0 , -2"));
     std::ostringstream os;
     os << v1;
     TS_ASSERT_EQUALS(os.str(), std::string("1 , 0 , -2"));
+
+    // Verify that the operands are not modified
+    TS_ASSERT_EQUALS(v1(1), 1.0);
+    TS_ASSERT_EQUALS(v1(2), 0.0);
+    TS_ASSERT_EQUALS(v1(3), -2.0);
   }
 };
