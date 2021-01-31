1eee4ea495b5054a6959d31cfe6802aa555f4cad
Bertrand Coconnier <bcoconni@users.sf.net>
2021-01-31 14:12:12 +0100

    Propagate the AUTOMATIC_DIFFERENTIATION flag to the Docker container.

--- python/build-wheels.sh
+++ python/build-wheels.sh
@@ -9,7 +9,7 @@ for PYBIN in /opt/python/*/bin; do
     # Skip deprecated or unsupported versions
     if "${PYBIN}/python" -c "import sys;sys.stdout.write(str(sys.version_info >= (${PYTHON_MIN_VERSION})))" | grep -q 'True'; then
         "${PYBIN}/pip" install cmake
-        "${PYBIN}/cmake" -DCMAKE_C_FLAGS_RELEASE="-g -O2 -DNDEBUG" -DCMAKE_CXX_FLAGS_RELEASE="-g -O2 -DNDEBUG" -DCMAKE_BUILD_TYPE=Release ..
+        "${PYBIN}/cmake" -DCMAKE_C_FLAGS_RELEASE="-g -O2 -DNDEBUG" -DCMAKE_CXX_FLAGS_RELEASE="-g -O2 -DNDEBUG" -DCMAKE_BUILD_TYPE=Release -DAUTOMATIC_DIFFERENTIATION=$AUTOMATIC_DIFFERENTIATION ..
         # Only build libJSBSim because that's all we need for Python wheels.
         "${PYBIN}/cmake" --build . --target libJSBSim
         break
