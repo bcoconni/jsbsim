bf975fadb736904ca34203e1f298e6eb676ae531
Bertrand Coconnier <bcoconni@users.sf.net>
2021-02-19 16:11:52 +0100

    Fix for FreeBSD

--- src/input_output/FGfdmSocket.cpp
+++ src/input_output/FGfdmSocket.cpp
@@ -40,7 +40,7 @@ INCLUDES
 
 #if defined(_MSC_VER) || defined(__MINGW32__)
 #include <ws2tcpip.h>
-#elif defined(__OpenBSD__)
+#elif defined(__OpenBSD__) || defined(__FreeBSD__)
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
