1a994104d7ce38e7819cfc80be6bea7a4b5dd7e5
Bertrand Coconnier <bcoconni@users.noreply.github.com>
2024-06-08 23:00:34 +0200

    `FGFCS` code clean up

--- src/models/FGFCS.h
+++ src/models/FGFCS.h
@@ -38,13 +38,7 @@ SENTRY
 INCLUDES
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%*/
 
-#include <iosfwd>
-#include <vector>
-#include <string>
-
 #include "FGFDMExec.h"
-#include "models/FGModel.h"
-#include "models/FGLGear.h"
 #include "models/FGGroundReactions.h"
 
 /*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -187,7 +181,6 @@ CLASS DECLARATION
 
 class JSBSIM_API FGFCS : public FGModel
 {
-
 public:
   /** Constructor
       @param Executive a pointer to the parent executive object */
@@ -222,7 +215,7 @@ public:
 
   /** Gets the steering command.
        @return steering command in range from -1.0 - 1.0 */
-   double GetDsCmd(void) const { return fdmex->GetGroundReactions()->GetDsCmd(); }
+   double GetDsCmd(void) const { return FDMExec->GetGroundReactions()->GetDsCmd(); }
 
   /** Gets the flaps command.
       @return flaps command in range from 0 to 1.0 */
@@ -391,7 +384,7 @@ public:
 
   /** Sets the steering command
        @param cmd steering command in percent*/
-   void SetDsCmd(double cmd) { fdmex->GetGroundReactions()->SetDsCmd( cmd ); }
+   void SetDsCmd(double cmd) { FDMExec->GetGroundReactions()->SetDsCmd( cmd ); }
 
   /** Sets the flaps command
       @param cmd flaps command in percent*/
@@ -587,10 +580,8 @@ private:
   double TailhookPos, WingFoldPos;
   SystemType systype;
   int ChannelRate;
-  FGFDMExec* fdmex;
 
-  typedef std::vector <FGFCSChannel*> Channels;
-  Channels SystemChannels;
+  std::vector<std::unique_ptr<FGFCSChannel>> SystemChannels;
   void bind(void);
   void bindThrottle(unsigned int);
   void Debug(int from) override;
