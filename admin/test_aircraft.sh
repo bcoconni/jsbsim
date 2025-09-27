74a62c8b4f114bf089ecc6df2b4dcfcf9c129d78
Bertrand Coconnier <bcoconni@users.noreply.github.com>
2025-09-27 18:03:18 +0200

    Fix the script.

--- admin/test_aircraft.sh
+++ admin/test_aircraft.sh
@@ -1,22 +1,23 @@
 #!/bin/bash
 for dir in $(ls -d aircraft/*); do
-name=$(basename "$dir")
-[ "$name" != "blank" ] || continue
-filename="${dir}/${name}.xml"
-[ -e "$filename" ] || continue
-xmllint --noout --schema JSBSim.xsd "$filename"
-
-if [ -d "${dir}/Systems" ]; then
-    for filename in "${dir}/Systems"/*.xml; do
-    [[ $filename != *_aero.xml ]] || continue
-    xmllint --noout --schema JSBSimSystem.xsd "$filename"
-    done
-fi
-
-for filename in ${dir}/*ap{,x}.xml; do
+    name=$(basename "$dir")
+    [ "$name" != "blank" ] || continue
+    filename="${dir}/${name}.xml"
     [ -e "$filename" ] || continue
-    xmllint --noout --schema aircraft/JSBSimAutopilot.xsd "$filename"
-done
+    xmllint --noout --schema JSBSim.xsd "$filename"
+
+    if [ -d "${dir}"/Systems ]; then
+        for filename in "${dir}"/Systems/*.xml; do
+            [ -e "$filename" ] || continue
+            [[ $filename != *_aero.xml ]] || continue
+            xmllint --noout --schema JSBSimSystem.xsd "$filename"
+        done
+    fi
+
+    for filename in "${dir}"/*ap{,x}.xml; do
+        [ -e "$filename" ] || continue
+        xmllint --noout --schema aircraft/JSBSimAutopilot.xsd "$filename"
+    done
 done
 
 xmllint --noout --schema JSBSimSystem.xsd aircraft/F450/Effectors.xml
